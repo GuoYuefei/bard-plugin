a300c166e2f435a9acfd884eaebd7c204848d250
GuoYuefei <beconegood@gmail.com>
2019-07-29 21:32:09 +0800

    可用

--- base/client/get_des_cfb_client.go
+++ base/client/get_des_cfb_client.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bard-plugin/base"
-	"fmt"
 )
 
 var V = base.Plugin{
@@ -12,24 +11,24 @@ var V = base.Plugin{
 	DESKEY: []byte("12345678"),
 	END_FLAG: []byte("\r\n\r\n"),
 }
-
-func main() {
-	var l int
-	ws := []byte("h23t43Keep-Alive\r\n\r\n\r\n\r\n4tp5453s://cctv.com12345678910")
-	fmt.Printf("%d \t  %s\n", len(ws), ws)
-
-	//ws, l = V.AntiSniffing(ws, true)
-	//fmt.Printf("a:\t%d \t  %s\n", l, ws)
-
-	ws, l = V.Camouflage(ws, true)
-	fmt.Printf("ac:\t%d \t  %s\n", l, ws)
-
-	_, l = V.Camouflage(ws, false)
-	fmt.Printf("a:\t%d \t  %s\n", l, ws[:l])
-
-	//_, l = V.AntiSniffing(ws[:l], false)
-	//fmt.Printf("%d \t %s\n", l, ws[:l])
-
-}
+//
+//func main() {
+//	var l int
+//	ws := []byte("h23t43Keep-Alive\r\n\r\n\r\n\r\n4tp5453s://cctv.com12345678910")
+//	fmt.Printf("%d \t  %s\n", len(ws), ws)
+//
+//	//ws, l = V.AntiSniffing(ws, true)
+//	//fmt.Printf("a:\t%d \t  %s\n", l, ws)
+//
+//	ws, l = V.Camouflage(ws, true)
+//	fmt.Printf("ac:\t%d \t  %s\n", l, ws)
+//
+//	_, l = V.Camouflage(ws, false)
+//	fmt.Printf("a:\t%d \t  %s\n", l, ws[:l])
+//
+//	//_, l = V.AntiSniffing(ws[:l], false)
+//	//fmt.Printf("%d \t %s\n", l, ws[:l])
+//
+//}
 
 
